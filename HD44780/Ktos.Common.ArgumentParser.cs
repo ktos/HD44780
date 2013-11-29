44fc2c65e7702bd8dabb8d8693bbb3bb7af02c6f
Marcin Badurowicz <ktos@ktos.info>
2013-11-29 20:49:46 +0100

    cosmetics

--- HD44780/Ktos.Common.ArgumentParser.cs
+++ HD44780/Ktos.Common.ArgumentParser.cs
@@ -1,27 +1,102 @@
-﻿using System;
+﻿#region License
+/*
+ * Hd44780
+ *
+ * Copyright (C) Marcin Badurowicz 2013
+ *
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files
+ * (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE. 
+ */
+#endregion
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Ktos.Common
 {
+    /// <summary>
+    /// A class representing single argument in command-line arguments:
+    ///
+    /// It may be something like: /s, -s, --silent
+    /// Or even: -s:something,something2
+    /// Or even: -s=something
+    /// Or: -s something something2
+    /// </summary>
     class Argument
     {
+        /// <summary>
+        /// Argument name (in the longer form, will be expanded if expanded list is populated)
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// List of argument switches
+        /// </summary>
         public List<string> Switches { get; set; }
     }
 
+    /// <summary>
+    /// A class parsing command-line argument
+    /// </summary>
     public class ArgumentParser
     {
-        private string[] args;        
+        /// <summary>
+        /// An pure string array argument list from application
+        /// </summary>
+        private string[] args;
+
+        /// <summary>
+        /// A list of expands for arguments - short form, expanded form
+        /// </summary>
         private Dictionary<string, string> expand;
 
+        /// <summary>
+        /// A list of parsed arguments
+        /// </summary>
         private List<Argument> parsedArguments;
 
+        /// <summary>
+        /// A "default" argument - when no argument is detected, all entries are going here
+        /// </summary>
         public static string DefaultArg = "--default";
+
+        /// <summary>
+        /// List of prefixes for recognizing an argument
+        /// </summary>
         public List<string> Prefixes { get; set; }
+
+        /// <summary>
+        /// List of prefixes for recognizing an argument's switch
+        /// </summary>
         public List<char> ParamPrefixes { get; set; }
+
+        /// <summary>
+        /// List of separators between argument's switches (except space)
+        /// </summary>
         public List<char> ParamSeparators { get; set; }
 
+        /// <summary>
+        /// Creates a new instance of ArgumentParser
+        /// </summary>
+        /// <param name="args">An argument array from command line</param>
         public ArgumentParser(string[] args)
         {
             this.args = args;
@@ -41,11 +116,19 @@ namespace Ktos.Common
             expand = new Dictionary<string, string>();
         }
 
+        /// <summary>
+        /// Adds expanded form for an argument
+        /// </summary>
+        /// <param name="shortSwitch">Short version, e.g. -s</param>
+        /// <param name="longSwitch">Long version, e.g. --silent</param>
         public void AddExpanded(string shortSwitch, string longSwitch)
         {
             expand.Add(shortSwitch, longSwitch);
         }
 
+        /// <summary>
+        /// Parses an argument list
+        /// </summary>
         public void Parse()
         {
             parsedArguments = new List<Argument>();
@@ -56,15 +139,14 @@ namespace Ktos.Common
 
             for (int i = 0; i < args.Length; i++)
             {
-                // jeśli zaczyna się od / albo -- albo - to uznajemy, że jest dany argument jest przełącznikiem
+                // if starts with /, -- or - it's probably a new argument
                 if (args[i].StartsWith("/") || args[i].StartsWith("--") || args[i].StartsWith("-"))
                 {
-                    // dodawanie poprzedniego przełącznika do listy już sparsowanych
+                    // actually parsed argument is added to parsed list, and we're creating a new one
                     parsedArguments.Add(actual);
                     
-                    // wydobycie "czystej" nazwy przełącznika, bez ewentualnych prefiksów parametrów
+                    // argument name
                     string newKey;
-
                     if (args[i].IndexOf(':') > -1)
                         newKey = args[i].Substring(0, args[i].IndexOf(':'));
                     else if (args[i].IndexOf('=') > -1)
@@ -72,9 +154,9 @@ namespace Ktos.Common
                     else
                         newKey = args[i];
                     
-                    // nowy przełącznik
+                    // creating a new one
                     actual = new Argument();
-                    actual.Name = ExpandSwitch(newKey);
+                    actual.Name = ExpandSwitch(newKey); // expanding name
                     actual.Switches = new List<string>();
 
                     foreach (var p in this.ParamPrefixes)
@@ -87,9 +169,8 @@ namespace Ktos.Common
                     }
                 }
                 else
-                // w przeciwnym wypadku dany argument linii polecenia jest parametrem przełącznika
-                {
-                    // rozszerzanie tablicy i dodawanie
+                // so it's not an argument, but a switch for previous argument
+                {                    
                     actual.Switches.Add(args[i]);
                 }                
             }
@@ -98,6 +179,11 @@ namespace Ktos.Common
             parsedArguments.Add(actual);
         }
 
+        /// <summary>
+        /// Expands an argument from short form to long form
+        /// </summary>
+        /// <param name="s">Short form of argument</param>
+        /// <returns>Long form of argument</returns>
         public string ExpandSwitch(string s)
         {
             string result = s;
@@ -115,6 +201,11 @@ namespace Ktos.Common
             }            
         }
 
+        /// <summary>
+        /// Gets a list of all switches for selected argument
+        /// </summary>
+        /// <param name="sw">Argument name</param>
+        /// <returns>Collection of all switches</returns>
         public IEnumerable<string> GetSwitchArguments(string sw)
         {
             sw = ExpandSwitch(sw);            
@@ -126,6 +217,11 @@ namespace Ktos.Common
                 throw new KeyNotFoundException("Specified switch not found");
         }
 
+        /// <summary>
+        /// Tests if required number of arguments is done
+        /// </summary>
+        /// <param name="requiredCount">A required number of arguments</param>
+        /// <returns>True if a number of arguments is greater than requiredCount</returns>
         public bool MatchRequiredCount(int requiredCount)
         {
             // jeśli jest tylko jeden, przełącznik znaczy "default", to zwracamy czy liczba jego
@@ -136,21 +232,39 @@ namespace Ktos.Common
                 return parsedArguments.Count >= requiredCount;
         }
 
+        /// <summary>
+        /// Checks if switch of a name exists
+        /// </summary>
+        /// <param name="sw">Switch name</param>
+        /// <returns>Returns true or false</returns>
         public bool SwitchExists(string sw)
         {
+            sw = this.ExpandSwitch(sw);
             return (parsedArguments.Find(x => x.Name == sw) != null);
         }
 
+        /// <summary>
+        /// Checks if help was requested
+        /// </summary>
+        /// <returns></returns>
         public bool HelpRequested()
         {
             return SwitchExists("--help");
         }
 
+        /// <summary>
+        /// Checks if version information was requested
+        /// </summary>
+        /// <returns></returns>
         public bool VersionRequested()
         {
             return SwitchExists("--version");
         }
 
+        /// <summary>
+        /// Checks if debug mode was requested
+        /// </summary>
+        /// <returns></returns>
         public bool DebugRequested()
         {
             return SwitchExists("--debug");
--- HD44780/Program.cs
+++ HD44780/Program.cs
@@ -1,10 +1,38 @@
-﻿using System;
+﻿#region License
+/*
+ * Hd44780
+ *
+ * Copyright (C) Marcin Badurowicz 2013
+ *
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files
+ * (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE. 
+ */
+#endregion
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//using RaspberryPiDotNet;
-//using RaspberryPiDotNet.MicroLiquidCrystal;
+using RaspberryPiDotNet;
+using RaspberryPiDotNet.MicroLiquidCrystal;
 using System.Threading;
 using Ktos.Common;
 
@@ -19,7 +47,7 @@ namespace Ktos.Hd44780
             pp.AddExpanded("-nl", "--no-light");
             pp.Parse();
 
-            /*var lcdProvider = new RaspPiGPIOMemLcdTransferProvider(GPIOPins.GPIO_07, GPIOPins.GPIO_08, GPIOPins.GPIO_25, GPIOPins.GPIO_24, GPIOPins.GPIO_23, GPIOPins.GPIO_18);
+            var lcdProvider = new RaspPiGPIOMemLcdTransferProvider(GPIOPins.GPIO_07, GPIOPins.GPIO_08, GPIOPins.GPIO_25, GPIOPins.GPIO_24, GPIOPins.GPIO_23, GPIOPins.GPIO_18);
             var lcd = new Lcd(lcdProvider);
 
             GPIOMem backlit = new GPIOMem(GPIOPins.GPIO_15, GPIODirection.Out);
@@ -27,18 +55,16 @@ namespace Ktos.Hd44780
 
             lcd.Begin(16, 2);
             lcd.Clear();
-            lcd.SetCursorPosition(0, 0);*/
+            lcd.SetCursorPosition(0, 0);
 
             if (pp.SwitchExists("--light"))
             {
-                //backlit.Write(true);
-                Console.WriteLine("light");
+                backlit.Write(true);                
             }
 
             if (pp.SwitchExists("--no-light"))
             {
-                //backlit.Write(true);
-                Console.WriteLine("nolight");
+                backlit.Write(false);                
             }
             
             string text;
@@ -47,12 +73,16 @@ namespace Ktos.Hd44780
                 var isKey = System.Console.KeyAvailable;
                 text = Console.ReadLine();
             }
-            catch (Exception e)
+            catch (InvalidOperationException)
             {
+                // when we're in piped output, InvalidOperationException is thrown for KeyAvaliable
+                // and we're using it here to check... dirty, dirty hack!
                 text = Console.In.ReadToEnd();
             }                
 
-            //lcd.Write(text);            
-        }        
+            lcd.Write(text);
+        }
+
+
     }
 }
